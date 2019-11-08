cd850e1d5e10bb382405752dd69e8f4066372298
Nicolas Boisteault <nboisteault@3liz.com>
2019-11-08 10:00:40 +0100

    Fix missing input creation.
    TODO : Let user active filter on specified columns.

--- attribute_table_column_filter/attribute_table_column_filter.js
+++ attribute_table_column_filter/attribute_table_column_filter.js
@@ -1,28 +1,31 @@
 lizMap.events.on({
-	attributeLayerContentReady: function(e) {
-		setTimeout(function(){
-			var cleanLayerName = lizMap.cleanName(e.featureType);
+    attributeLayerContentReady: function (e) {
+        var cleanLayerName = lizMap.cleanName(e.featureType);
 
-			if( $('#attribute-layer-table-'+cleanLayerName+'_wrapper').data('filtersON') == undefined ){
-				// Set flag to add filters only once
-				$('#attribute-layer-table-'+cleanLayerName+'_wrapper').data('filtersON', true);
+        if ($('#attribute-layer-table-' + cleanLayerName + '_wrapper').data('filtersON') === undefined) {
+            // Set flag to add filters only once
+            $('#attribute-layer-table-' + cleanLayerName + '_wrapper').data('filtersON', true);
 
-				// You can put an array of index in columns() to tell on which column you want the search input to appear
-				$('#attribute-layer-table-'+cleanLayerName).dataTable().api().columns().every( function () {
-			       	var column = this;
+            $('#attribute-layer-table-' + cleanLayerName + '_wrapper thead:first th').not('.sorting_disabled').each(function () {
+                var title = $(this).text();
+                $(this).html('<input type="text" placeholder=" ' + title + '" />');
+            });
 
-			       	$( 'input', this.header() ).on( 'keyup change', function () {
-						if ( column.search() !== this.value ) {
-						   column
-						       .search( this.value )
-						       .draw();
-						}
-			       	}).click(function(e) {// We don't want to sort when users click on the search field
-						e.stopPropagation();
-					});
-				});
-				lizMap.refreshDatatableSize("#attribute-layer-main-"+cleanLayerName);
-			}
-		}, 500);
-	}
+            $('#attribute-layer-table-' + cleanLayerName).DataTable().columns().every(function () {
+                var column = this;
+
+                $('input', this.header()).on('keyup change', function () {
+                    if (column.search() !== this.value) {
+                        column
+                            .search(this.value)
+                            .draw();
+                    }
+                }).click(function (e) {
+                    // We don't want to sort when users click on the search field
+                    e.stopPropagation();
+                });
+            });
+            lizMap.refreshDatatableSize("#attribute-layer-main-" + cleanLayerName);
+        }
+    }
 });
