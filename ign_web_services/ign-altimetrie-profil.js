9114bac078b4d9ffd9d12293ef4b1568103f06d5
rldhont <rldhont@gmail.com>
2020-01-19 18:47:26 +0100

    Fix ign web services scripts

--- ign_web_services/ign-altimetrie-profil.js
+++ ign_web_services/ign-altimetrie-profil.js
@@ -1,123 +1,132 @@
-var ignServiceKey;
-var ignServiceUrl;
-var ignEntryPoints;
-var scriptPath = document.currentScript;
+var lizmapIgnAltimetrieProfil = function() {
+    //IGN KEYS MUST BE DEFINED inside credencial json file !
+    var ignServiceKey;
+    var ignServiceUrl;
+    var ignEntryPoints;
+    var scriptPath = document.currentScript;
 
-lizMap.events.on({
-        'uicreated': function(e) {
-		lizMap.addDock('ign_altimetrie_profil', 'Profil Altimétrique', 'minidock', 'Veuillez cliquer sur la carte <br /> pour définir un point de départ un point d\'arrivé', 'icon-screenshot');
-		credencial = getCredentialPath();
-		$.getJSON(credencial, function(json) {
-			ignServiceKey = json.ignServiceKey;
-			ignServiceUrl = json.ignServiceUrl;
-			ignEntryPoints = json.services.alti.ignEntryPoints;	
-			initIgnAltiProfilView();					
-		});						
-	}
-});
+    lizMap.events.on({
+            'uicreated': function(e) {
+            lizMap.addDock('ign_altimetrie_profil', 'Profil Altimétrique', 'minidock', 'Veuillez cliquer sur la carte <br /> pour définir un point de départ un point d\'arrivé', 'icon-screenshot');
+            credencial = getCredentialPath();
+            $.getJSON(credencial, function(json) {
+                ignServiceKey = json.ignServiceKey;
+                ignServiceUrl = json.ignServiceUrl;
+                ignEntryPoints = json.services.alti.ignEntryPoints;
+                initIgnAltiProfilView();
+            });
+        }
+    });
 
-function getCredentialPath(){
-	var splitScriptPath  = scriptPath.src.split("%2F");
-	var jsDirPath = splitScriptPath.slice(0, splitScriptPath.length - 1).join("/") + "/";
-	var credencial = jsDirPath.concat('credencial.json');
-	return credencial;
-}
+    function getCredentialPath(){
+        var splitScriptPath  = scriptPath.src.split("%2F");
+        var jsDirPath = splitScriptPath.slice(0, splitScriptPath.length - 1).join("/") + "/";
+        var credencial = jsDirPath.concat('credencial.json');
+        return credencial;
+    }
 
-function getIgnJsonResponse(service, params, aCallback){	
-	var fullUrl = '';
-	var ep = ignEntryPoints;
-	var fullUrl = ignServiceUrl + ignServiceKey + ep;    
-	$.get(
-		fullUrl,
-		params,
-		function(data) {        	
-			if(aCallback){
-	    			aCallback(data);			
-			}
-		}
-		,'json'
-	);
+    function getIgnJsonResponse(service, params, aCallback){
+        var fullUrl = '';
+        var ep = ignEntryPoints;
+        var fullUrl = ignServiceUrl + ignServiceKey + ep;
+        $.get(
+            fullUrl,
+            params,
+            function(data) {
+                if(aCallback){
+                        aCallback(data);
+                }
+            }
+            ,'json'
+        );
 
-}
+    }
 
-function initIgnAltiView() {
-	var map = lizMap.map;
+    function initIgnAltiView() {
+        var map = lizMap.map;
 
-	 var html = '<button">Réinitialiser</button>';
-	html.insertAfter( $('#ign_altimetrie_profil ign_altimetrie_profil h3') )  ;
-	
-	//Layer to display clic location
-	var ignLayerAlti = map.getLayersByName('ignLayerAlti');
-	if ( ignLayerAlti.length == 0 ) {
-		ignLayerAlti = new OpenLayers.Layer.Vector('ignLayerAlti',{
-			styleMap: new OpenLayers.StyleMap({
-				graphicName: 'cross',
-				pointRadius: 6,
-				fill: true,
-				fillColor: 'white',
-				fillOpacity: 1,
-				stroke: true,
-				strokeWidth: 2,
-				strokeColor: 'red',
-				strokeOpacity: 1
-			})
-		});
-		map.addLayer(ignLayerAlti);
-		ignLayerAlti.setVisibility(true);
-	}	
+         var html = '<button">Réinitialiser</button>';
+        html.insertAfter( $('#ign_altimetrie_profil ign_altimetrie_profil h3') )  ;
 
-	OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {                
-		defaultHandlerOptions: {
-		    'single': true,
-		    'double': false,
-		    'pixelTolerance': 0,
-		    'stopSingle': false,
-		    'stopDouble': false
-		},
+        //Layer to display clic location
+        var ignLayerAlti = map.getLayersByName('ignLayerAlti');
+        if ( ignLayerAlti.length == 0 ) {
+            ignLayerAlti = new OpenLayers.Layer.Vector('ignLayerAlti',{
+                styleMap: new OpenLayers.StyleMap({
+                    graphicName: 'cross',
+                    pointRadius: 6,
+                    fill: true,
+                    fillColor: 'white',
+                    fillOpacity: 1,
+                    stroke: true,
+                    strokeWidth: 2,
+                    strokeColor: 'red',
+                    strokeOpacity: 1
+                })
+            });
+            map.addLayer(ignLayerAlti);
+            ignLayerAlti.setVisibility(true);
+        }
 
-		initialize: function(options) {
-		    this.handlerOptions = OpenLayers.Util.extend(
-			{}, this.defaultHandlerOptions
-		    );
-		    OpenLayers.Control.prototype.initialize.apply(
-			this, arguments
-		    ); 
-		    this.handler = new OpenLayers.Handler.Click(
-			this, {
-			    'click': this.trigger
-			}, this.handlerOptions
-		    );
-		}, 
+        OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
+            defaultHandlerOptions: {
+                'single': true,
+                'double': false,
+                'pixelTolerance': 0,
+                'stopSingle': false,
+                'stopDouble': false
+            },
 
-		trigger: function(e) {
-			ignLayerAlti.destroyFeatures();
-			$('#ign_altimetrie .menu-content').html( 'Altitude :'.concat(' ', '...') );
-			var lonlat = map.getLonLatFromPixel(e.xy);			
-			ignLayerAlti.addFeatures([ 
-				new OpenLayers.Feature.Vector(
-					new OpenLayers.Geometry.Point(lonlat.lon, lonlat.lat)  
-				)
-			]);
-			getIgnAlti(lonlat.lon,lonlat.lat)	
-		}
+            initialize: function(options) {
+                this.handlerOptions = OpenLayers.Util.extend(
+                {}, this.defaultHandlerOptions
+                );
+                OpenLayers.Control.prototype.initialize.apply(
+                this, arguments
+                );
+                this.handler = new OpenLayers.Handler.Click(
+                this, {
+                    'click': this.trigger
+                }, this.handlerOptions
+                );
+            },
 
-	});
-	var click = new OpenLayers.Control.Click();
-	map.addControl(click);        
+            trigger: function(e) {
+                ignLayerAlti.destroyFeatures();
+                $('#ign_altimetrie .menu-content').html( 'Altitude :'.concat(' ', '...') );
+                var lonlat = map.getLonLatFromPixel(e.xy);
+                ignLayerAlti.addFeatures([
+                    new OpenLayers.Feature.Vector(
+                        new OpenLayers.Geometry.Point(lonlat.lon, lonlat.lat)
+                    )
+                ]);
+                getIgnAlti(lonlat.lon,lonlat.lat)
+            }
 
-	lizMap.events.on({
-		minidockopened: function(e) {
-		    	if ( e.id == 'ign_altimetrie' ) {							
-				click.activate();
-		    	}
-		},
-			minidockclosed: function(e) {
-		    	if ( e.id == 'ign_altimetrie' ) {
-				ignLayerAlti.destroyFeatures();	
-				ignLayerAlti.setVisibility(false);		
-				click.deactivate();
-		    	}
-		}
-	});
+        });
+        var click = new OpenLayers.Control.Click();
+        map.addControl(click);
 
-}
+        lizMap.events.on({
+            minidockopened: function(e) {
+                    if ( e.id == 'ign_altimetrie' ) {
+                    click.activate();
+                    }
+            },
+                minidockclosed: function(e) {
+                    if ( e.id == 'ign_altimetrie' ) {
+                    ignLayerAlti.destroyFeatures();
+                    ignLayerAlti.setVisibility(false);
+                    click.deactivate();
+                    }
+            }
+        });
+
+    }
+
+    return {
+        'serviceKey': ignServiceKey,
+        'serviceUrl': ignServiceUrl,
+        'entryPoints': ignEntryPoints
+    };
+}();
--- ign_web_services/ign-altimetrie.js
+++ ign_web_services/ign-altimetrie.js
@@ -1,145 +1,153 @@
-//IGN KEYS MUST BE DEFINED inside credencial json file !
-var ignServiceKey;
-var ignServiceUrl;
-var ignEntryPoints;
-var help = 'Veuillez cliquer sur la carte <br />pour connaitre l\'altitude';
+var lizmapIgnAltimetrie = function() {
+    //IGN KEYS MUST BE DEFINED inside credencial json file !
+    var ignServiceKey;
+    var ignServiceUrl;
+    var ignEntryPoints;
+    var help = 'Veuillez cliquer sur la carte <br />pour connaitre l\'altitude';
 
-lizMap.events.on({
-        'uicreated': function(e) {
-		lizMap.addDock('ign_altimetrie', 'Altimétrie', 'minidock', help, 'icon-screenshot');
-		var credencial = OpenLayers.Util.urlAppend(
-		     lizUrls.media,
-		     OpenLayers.Util.getParameterString({
-			 "repository": lizUrls.params.repository,
-			 "project": lizUrls.params.project,
-			 "path": "media/js/".concat(lizUrls.params.project,"/credencial.json")
-		     })
-		);
-		
-		$.getJSON(credencial, function(json) {
-			
-			ignServiceKey = json.ignServiceKey;
-			ignServiceUrl = json.ignServiceUrl;
-			ignEntryPoints = json.services.alti.ignEntryPoints;	
-			
-			initIgnAltiView();					
-		});						
-	}
-});
+    lizMap.events.on({
+            'uicreated': function(e) {
+            lizMap.addDock('ign_altimetrie', 'Altimétrie', 'minidock', help, 'icon-screenshot');
+            var credencial = OpenLayers.Util.urlAppend(
+                 lizUrls.media,
+                 OpenLayers.Util.getParameterString({
+                 "repository": lizUrls.params.repository,
+                 "project": lizUrls.params.project,
+                 "path": "media/js/".concat(lizUrls.params.project,"/credencial.json")
+                 })
+            );
 
-function getIgnJsonResponse(service, params, aCallback){	
-	var fullUrl = '';
-	var ep = ignEntryPoints;
-	var fullUrl = ignServiceUrl + ignServiceKey + ep;    
-	$.get(
-		fullUrl,
-		params,
-		function(data) {        	
-			if(aCallback){
-	    			aCallback(data);			
-			}
-		}
-		,'json'
-	);
-}
+            $.getJSON(credencial, function(json) {
 
-function getIgnAlti(lon,lat){	
-	//IGN Web Service only allows coordinates in 4326
-	if(lizMap.map.projection.projCode != "EPSG:4326"){
-		var fromProjection = new OpenLayers.Projection(lizMap.map.projection.projCode);
-		var toProjection = new OpenLayers.Projection("EPSG:4326");
-		var convertedPoint = new OpenLayers.LonLat(lon, lat);
-		convertedPoint.transform(fromProjection, toProjection);
-		lon = convertedPoint.lon;
-		lat = convertedPoint.lat;
-	}
-	
-	var qParams = {
-		'lon': lon,
-		'lat':lat,	
-		'srs': lizMap.map.projection.projCode
-	}
-	getIgnJsonResponse('alti', qParams, function(data){
-		var alt = data['elevations'][0]['z'];
-		$('#ign_altimetrie .menu-content').html('Altitude :'.concat(' ',alt));		
-	});
-}
+                ignServiceKey = json.ignServiceKey;
+                ignServiceUrl = json.ignServiceUrl;
+                ignEntryPoints = json.services.alti.ignEntryPoints;
 
-function initIgnAltiView() {
-	var map = lizMap.map;
-	
-	//Layer to display clic location
-	var ignLayerAlti = map.getLayersByName('ignLayerAlti');
-	if ( ignLayerAlti.length == 0 ) {
-		ignLayerAlti = new OpenLayers.Layer.Vector('ignLayerAlti',{
-			styleMap: new OpenLayers.StyleMap({
-				graphicName: 'cross',
-				pointRadius: 6,
-				fill: true,
-				fillColor: 'white',
-				fillOpacity: 1,
-				stroke: true,
-				strokeWidth: 2,
-				strokeColor: 'red',
-				strokeOpacity: 1
-			})
-		});
-		map.addLayer(ignLayerAlti);
-		ignLayerAlti.setVisibility(true);
-	}	
+                initIgnAltiView();
+            });
+        }
+    });
 
-	OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {                
-		defaultHandlerOptions: {
-		    'single': true,
-		    'double': false,
-		    'pixelTolerance': 0,
-		    'stopSingle': false,
-		    'stopDouble': false
-		},
+    function getIgnJsonResponse(service, params, aCallback){
+        var fullUrl = '';
+        var ep = ignEntryPoints;
+        var fullUrl = ignServiceUrl + ignServiceKey + ep;
+        $.get(
+            fullUrl,
+            params,
+            function(data) {
+                if(aCallback){
+                        aCallback(data);
+                }
+            }
+            ,'json'
+        );
+    }
 
-		initialize: function(options) {
-		    this.handlerOptions = OpenLayers.Util.extend(
-			{}, this.defaultHandlerOptions
-		    );
-		    OpenLayers.Control.prototype.initialize.apply(
-			this, arguments
-		    ); 
-		    this.handler = new OpenLayers.Handler.Click(
-			this, {
-			    'click': this.trigger
-			}, this.handlerOptions
-		    );
-		}, 
+    function getIgnAlti(lon,lat){
+        //IGN Web Service only allows coordinates in 4326
+        if(lizMap.map.projection.projCode != "EPSG:4326"){
+            var fromProjection = new OpenLayers.Projection(lizMap.map.projection.projCode);
+            var toProjection = new OpenLayers.Projection("EPSG:4326");
+            var convertedPoint = new OpenLayers.LonLat(lon, lat);
+            convertedPoint.transform(fromProjection, toProjection);
+            lon = convertedPoint.lon;
+            lat = convertedPoint.lat;
+        }
 
-		trigger: function(e) {
-			ignLayerAlti.destroyFeatures();
-			$('#ign_altimetrie .menu-content').html( 'Altitude :'.concat(' ', '...') );
-			var lonlat = map.getLonLatFromPixel(e.xy);			
-			ignLayerAlti.addFeatures([ 
-				new OpenLayers.Feature.Vector(
-					new OpenLayers.Geometry.Point(lonlat.lon, lonlat.lat)  
-				)
-			]);
-			getIgnAlti(lonlat.lon,lonlat.lat)	
-		}
+        var qParams = {
+            'lon': lon,
+            'lat':lat,
+            'srs': lizMap.map.projection.projCode
+        }
+        getIgnJsonResponse('alti', qParams, function(data){
+            var alt = data['elevations'][0]['z'];
+            $('#ign_altimetrie .menu-content').html('Altitude :'.concat(' ',alt));
+        });
+    }
 
-	});
-	var click = new OpenLayers.Control.Click();
-	map.addControl(click);        
+    function initIgnAltiView() {
+        var map = lizMap.map;
 
-	lizMap.events.on({
-		minidockopened: function(e) {
-		    	if ( e.id == 'ign_altimetrie' ) {							
-				click.activate();
-		    	}
-		},
-			minidockclosed: function(e) {
-		    	if ( e.id == 'ign_altimetrie' ) {
-				$('#ign_altimetrie .menu-content').html(help);
-				ignLayerAlti.destroyFeatures();	
-				ignLayerAlti.setVisibility(false);		
-				click.deactivate();				
-		    	}
-		}
-	});
-}
+        //Layer to display clic location
+        var ignLayerAlti = map.getLayersByName('ignLayerAlti');
+        if ( ignLayerAlti.length == 0 ) {
+            ignLayerAlti = new OpenLayers.Layer.Vector('ignLayerAlti',{
+                styleMap: new OpenLayers.StyleMap({
+                    graphicName: 'cross',
+                    pointRadius: 6,
+                    fill: true,
+                    fillColor: 'white',
+                    fillOpacity: 1,
+                    stroke: true,
+                    strokeWidth: 2,
+                    strokeColor: 'red',
+                    strokeOpacity: 1
+                })
+            });
+            map.addLayer(ignLayerAlti);
+            ignLayerAlti.setVisibility(true);
+        }
+
+        OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
+            defaultHandlerOptions: {
+                'single': true,
+                'double': false,
+                'pixelTolerance': 0,
+                'stopSingle': false,
+                'stopDouble': false
+            },
+
+            initialize: function(options) {
+                this.handlerOptions = OpenLayers.Util.extend(
+                {}, this.defaultHandlerOptions
+                );
+                OpenLayers.Control.prototype.initialize.apply(
+                this, arguments
+                );
+                this.handler = new OpenLayers.Handler.Click(
+                this, {
+                    'click': this.trigger
+                }, this.handlerOptions
+                );
+            },
+
+            trigger: function(e) {
+                ignLayerAlti.destroyFeatures();
+                $('#ign_altimetrie .menu-content').html( 'Altitude :'.concat(' ', '...') );
+                var lonlat = map.getLonLatFromPixel(e.xy);
+                ignLayerAlti.addFeatures([
+                    new OpenLayers.Feature.Vector(
+                        new OpenLayers.Geometry.Point(lonlat.lon, lonlat.lat)
+                    )
+                ]);
+                getIgnAlti(lonlat.lon,lonlat.lat)
+            }
+
+        });
+        var click = new OpenLayers.Control.Click();
+        map.addControl(click);
+
+        lizMap.events.on({
+            minidockopened: function(e) {
+                    if ( e.id == 'ign_altimetrie' ) {
+                    click.activate();
+                    }
+            },
+                minidockclosed: function(e) {
+                    if ( e.id == 'ign_altimetrie' ) {
+                    $('#ign_altimetrie .menu-content').html(help);
+                    ignLayerAlti.destroyFeatures();
+                    ignLayerAlti.setVisibility(false);
+                    click.deactivate();
+                    }
+            }
+        });
+    }
+
+    return {
+        'serviceKey': ignServiceKey,
+        'serviceUrl': ignServiceUrl,
+        'entryPoints': ignEntryPoints
+    };
+}();
