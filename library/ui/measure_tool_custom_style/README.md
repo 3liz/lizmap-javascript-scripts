dfc017868743541236422703eb3eaf02d7cb33ac
nboisteault <nboisteault@3liz.com>
2022-06-20 17:56:59 +0200

    Measure tool custom style

---
+++ library/ui/measure_tool_custom_style/README.md
@@ -0,0 +1,3 @@
+## Purpose
+
+Define the stroke color for all measure tools in `customStrokeColor` or you are free to set style rules individually in `customSymbolizer`.
---
+++ library/ui/measure_tool_custom_style/measure_tool_custom_style.js
@@ -0,0 +1,39 @@
+lizMap.events.on({
+
+    minidockopened : e => {
+        if(e.id === "measure"){
+            // Define the stroke color for all measure tools in `customStrokeColor`
+            // or you are free to set style rules individually in `customSymbolizer`
+            const customStrokeColor = '#0000ff';
+            const customSymbolizer = {
+                "Point": {
+                    pointRadius: 4,
+                    graphicName: "square",
+                    fillColor: "white",
+                    fillOpacity: 1,
+                    strokeWidth: 1,
+                    strokeOpacity: 1,
+                    strokeColor: customStrokeColor
+                },
+                "Line": {
+                    strokeWidth: 3,
+                    strokeOpacity: 1,
+                    strokeColor: customStrokeColor,
+                    strokeDashstyle: "dash"
+                },
+                "Polygon": {
+                    strokeWidth: 2,
+                    strokeOpacity: 1,
+                    strokeColor: customStrokeColor,
+                    strokeDashstyle: "dash",
+                    fillColor: "white",
+                    fillOpacity: 0.3
+                }
+            };
+
+            for (const measureControl of lizMap.map.getControlsByClass('OpenLayers.Control.Measure')) {
+                measureControl.handlerOptions.layerOptions.styleMap.styles.default.rules[0].symbolizer = customSymbolizer;
+            }
+        }
+    }
+});
