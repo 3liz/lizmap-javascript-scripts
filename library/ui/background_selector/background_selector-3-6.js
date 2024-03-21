6184ab99d0c3fe2e0ee684b3f288dc8b573d07f5
npltr62 <40625762+npltr62@users.noreply.github.com>
2024-03-21 10:31:57 +0100

    background_selector improvements (#71)
    * background_selector improvements
    Fixes #57
    
    * add legacy script for 3.6
    * update README
    * update CSS for 3.7
    
    ---------
    
    Co-authored-by: rmartin <rmartin@3liz.com>

--- library/ui/background_selector/background_selector-3-6.js
+++ library/ui/background_selector/background_selector-3-6.js
@@ -1,30 +1,32 @@
 lizMap.events.on({
 
     'uicreated':function(evt){
-        /**
-         * diff 3.6 -> 3.7 : replace #switcher-baselayer-select by lizmap-base-layers select
-         * and change lizMap.mainLizmap.state (see below)
-         */
+
         function addBaselayerImageSelector(){
-            var activebl = $('#switcher-baselayer lizmap-base-layers select').val();
+            var activebl = $('#switcher-baselayer-select').val();
             var html = '<div id="baselayer-image-selector" class="'+activebl+'" title="Changer le fond de carte">';
             html+= '&nbsp;';
             html+= '</div>';
+
             //$('#map-content').append(html);
             if($('#baselayer-image-selector').length == 0){
                 $('#map-content').append(html);
+
+                if($('#overview-box').length == 1)
+                    $('#baselayer-image-selector').addClass('with-overview-box');
+
                 $('#baselayer-image-selector').tooltip({placement: 'bottom'});
                 $('#baselayer-image-selector').click(function(){
 
                     // Get next baselayer
-                    var nextbl = $('lizmap-base-layers select option:selected').next('option');
+                    var nextbl = $('#switcher-baselayer-select option:selected').next('option');
                     if(nextbl.length == 0)
-                        nextbl = $('lizmap-base-layers select option:first');
+                        nextbl = $('#switcher-baselayer-select option:first');
                     var nextbl_val = nextbl.attr('value');
+
                     // Change baselayer
-                     $('lizmap-base-layers select').val(nextbl_val).change();
-                     //!!!Important Set baselayer in lizMap config
-                    lizMap.mainLizmap.state.baseLayers.selectedBaseLayerName = nextbl_val
+                     $('#switcher-baselayer-select').val(nextbl_val).change();
+
                     // Change background image of next possible
                     getNextBaselayerImage();
 
@@ -35,15 +37,16 @@ lizMap.events.on({
         }
 
         function getNextBaselayerImage(){
-            var nextbl = $('lizmap-base-layers select option:selected').next('option');
+            var nextbl = $('#switcher-baselayer-select option:selected').next('option');
             if(nextbl.length == 0)
-                nextbl = $('lizmap-base-layers select option:first');
+                nextbl = $('#switcher-baselayer-select option:first');
             var nextbl_text = nextbl.text();
             var nextbl_val = nextbl.attr('value');
             var cur_image_url = $('#baselayer-image-selector').css('background-image');
             var cur_image_spl = cur_image_url.split('/');
             cur_image_spl.pop();
             var nextbl_image = cur_image_spl.join('/') + '/' + nextbl_val + '.png';
+
             $('#baselayer-image-selector')
                 .css(
                     'background-image',
