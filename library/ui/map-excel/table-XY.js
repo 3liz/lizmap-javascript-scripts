4ec702b9c062f8fac6f6b4d6de90cca33667954b
Althéa Feuillet <123597411+altheaFeu@users.noreply.github.com>
2024-03-26 17:23:06 +0100

    Update table-XY.js
    Update for the worker

--- library/ui/map-excel/table-XY.js
+++ library/ui/map-excel/table-XY.js
@@ -1,51 +1,62 @@
-function startReadingXY(promises, fieldLon, fieldLat) {
+function startReadingXY(promises) {
     Promise.all(promises)
         .then(dataJson => {
-            if (dataJson.length > 0) {
-                var map = lizMap.map;
-
-                iconStyle = createIconStyle($('#selectLogo').val());
-
-                layerXY = new OpenLayers.Layer.Vector('GPSLayer', {
-                    styleMap: iconStyle
-                });
-
-                map.addLayer(layerXY);
-
-                dataJson.forEach(table => {
-                    table.forEach(rowData => {
-                        var lon = parseFloat(rowData[fieldLon]);
-                        var lat = parseFloat(rowData[fieldLat]);
-
-                        feature = createPoint(lon, lat, 'EPSG:4326');
-
-                        feature.attributes = rowData;
-                        layerXY.addFeatures(feature);
-
-                        layerXY.events.on({
-                            'featureselected': function (event) {
-                                var feature = event.feature;
-                                var content = createPopupContent(feature.attributes);
-                                var popup = new OpenLayers.Popup.FramedCloud(
-                                    'popup',
-                                    feature.geometry.getBounds().getCenterLonLat(),
-                                    null,
-                                    content,
-                                    null,
-                                    true,
-                                    null
-                                );
-                                map.addPopup(popup);
-                            }
-                        });
-                    })
-                })
-
-            } else {
-                alert("No data to map");
-            }
-        }).catch(error => {
-            alert("Error");
-            console.log(error);
+            workerXY.postMessage({ dataJson });
+        })
+        .catch(error => {
+            console.error(error);
         });
+}
+
+// Ecoute du message provenant du web worker
+workerXY.onmessage = function(event) {
+    const { dataJson } = event.data;
+    const colLon = $('#selectLongitude').val();
+    const colLat = $('#selectLatitude').val();
+
+    if (dataJson.length > 0) {
+        var map = lizMap.map;
+
+        iconStyle = createIconStyle($('#selectLogo').val());
+
+        layerXY = new OpenLayers.Layer.Vector('LayerGPS', {
+            styleMap: iconStyle
+        });
+
+        map.addLayer(layerXY);
+
+        dataJson.forEach(table => {
+            table.forEach(rowData => {
+                var lon = parseFloat(rowData[colLon]);
+                var lat = parseFloat(rowData[colLat]);
+
+                feature = createPoint(lon, lat, 'EPSG:4326');
+
+                feature.attributes = rowData;
+                layerXY.addFeatures(feature);
+
+                layerXY.events.on({
+                    'featureselected': function (event) {
+                        var feature = event.feature;
+                        var content = createPopupContent(feature.attributes);
+                        var popup = new OpenLayers.Popup.FramedCloud(
+                            'popup',
+                            feature.geometry.getBounds().getCenterLonLat(),
+                            null,
+                            content,
+                            null,
+                            true,
+                            null
+                        );
+                        map.addPopup(popup);
+                    }
+                });
+            })
+        })
+
+        workerXY.terminate();
+
+    } else {
+        alert("No data to map");
+    }
 }
