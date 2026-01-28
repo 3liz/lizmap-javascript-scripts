e343acfb592bea31e495b2de8229547f834f3f81
Michael Douchin <mdouchin@3liz.com>
2026-01-28 16:43:44 +0100

    Refresh layers - Modernize JS code

--- library/misc/refresh_layers_every_n_seconds/refresh_layers_every_n_seconds_3.9.js
+++ library/misc/refresh_layers_every_n_seconds/refresh_layers_every_n_seconds_3.9.js
@@ -4,39 +4,102 @@
  * There isn't any guarantee that this script will work on another version of Lizmap Web Client.
  */
 
-lizMap.events.on({
-    uicreated: () => {
-        // List of layer QGIS names to refresh
-        const refreshLayersName = [
-            'Quartiers',
-            'SousQuartiers'
-        ];
+var lizRefreshLayers = function () {
 
-        // Refresh interval in milliseconds
-        const refreshInterval = 10000;
+    // =================
+    //  CONFIGURATION
+    // =================
 
-        // ****
-        // Do not edit below
-        // ****
+    // List of layer QGIS names to refresh
+    // Write the exact names as in QGIS project
+    const QGIS_LAYER_NAMES = [
+        'Quartiers',
+        'SousQuartiers',
+    ];
 
-        // Get OL layers by name
-        const olLayers = [];
-        const olMap = lizMap.mainLizmap?.map;
-        for (const layerName of refreshLayersName) {
-            const olLayer = olMap.getLayerByName(layerName);
-            olLayers.push(olLayer);
-        }
+    // Refresh interval in milliseconds
+    const REFRESH_INTERVAL_MS = 30000;
 
-        // Refresh all given layers
-        function refreshLayers() {
-            for (const olLayer of olLayers) {
-                if (olLayer.getVisible()) {
-                    olLayer.getSource().changed();
-                }
+    // =================
+    // END CONFIGURATION
+    // Do not edit below
+    // =================
+
+    // Array of OpenLayers layers to refresh
+    // Filled during initialization
+    const OL_LAYERS = [];
+
+    /**
+     * Refresh visible layers
+     *
+     * @param {Array} olLayers - Array of OpenLayers layers to refresh
+     */
+    function refreshLayers(olLayers = []) {
+        // console.log('lizRefreshLayers - Refreshing layers');
+        // console.log(olLayers);
+        for (var l in olLayers) {
+            const layer = olLayers[l];
+            if (layer.getVisible()) {
+                layer.getSource().refresh();
             }
         }
+    }
+
+    /**
+     * Initialize the refresh process
+     * @returns {boolean}
+     */
+    function init() {
+        // Find OpenLayers layers from QGIS layer names
+        var olLayers = [];
+        for (var i in QGIS_LAYER_NAMES) {
+            const layerName = QGIS_LAYER_NAMES[i];
+            const olLayer = lizMap.mainLizmap.map.getLayerByName(layerName)
+            if (olLayer) {
+                olLayers.push(olLayer);
+            }
+        }
+        if (olLayers.length === 0) {
+            console.warn('lizRefreshLayers - No layers found to refresh');
+            return true;
+        }
+        OL_LAYERS.push(...olLayers);
 
         // Set timer to refresh layers every N milliseconds
-        setInterval(refreshLayers, refreshInterval);
+        (function loop() {
+        setTimeout(() => {
+            refreshLayers(olLayers),
+            loop();
+        }, REFRESH_INTERVAL_MS);
+        })();
     }
-});
+
+    /**
+     * Wait for lizMap and mainLizmap objects to be available
+     * @returns {Promise<boolean>}
+     */
+    function waitForObject() {
+        return new Promise((resolve, reject) => {
+            const intervalId = setInterval(() => {
+                if (lizMap && lizMap.mainLizmap && lizMap.mainLizmap.map) {
+                    clearInterval(intervalId);
+                    resolve(true);
+                }
+            }, 200); // Check every 200 milliseconds for availability of listed objects
+        });
+    }
+
+    // Start when lizMap and mainLizmap are available
+    waitForObject().then((response) => {
+        init();
+    });
+
+    // Public API
+    let obj = {
+        olLayers: OL_LAYERS,
+        refreshLayers: refreshLayers,
+    }
+
+    return obj;
+
+}();
